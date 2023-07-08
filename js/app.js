dd2c7a8307d70dcc3243c0cafe07d67f663de3db
tgoodall3 <Tylergoodall3@gmail.com>
2023-07-07 21:09:17 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+function setup(){
+    createCanvas(800,600);
+}
+
+function draw(){
+    background(200)
+    var x = 100;
+
+    for(var i=0; i<1250; i += 50) {
+        fill(0);
+        ellipse(i,x,50,50);
+
+        if (i % 3 == 0){
+        fill(153,30,240);
+        ellipse(i,x,50,50);
+        }
+
+
+    if(i % 5 == 0) {
+    fill(0,255,0);
+    square(i + 25, x - 25 ,50);
+    }
+
+    if (i % 3 == 0 && i % 5 == 0){
+        fill(0,0,225);
+        square(i + 25, x - 25, 50);
+    }
+}
+}
