5586353f4489264fb888e1c5a2dec4c37c4b8c61
tgoodall3 <Tylergoodall3@gmail.com>
2023-07-07 21:23:20 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,17 +13,20 @@ function draw(){
         ellipse(i,x,50,50);
 
         if (i % 3 == 0){
+            //if divisible by 3 = purple circle
         fill(153,30,240);
         ellipse(i,x,50,50);
         }
 
 
     if(i % 5 == 0) {
+        // if divisible by 5 = green square
     fill(0,255,0);
     square(i + 25, x - 25 ,50);
     }
 
     if (i % 3 == 0 && i % 5 == 0){
+        // if divisble by both = blue square
         fill(0,0,225);
         square(i + 25, x - 25, 50);
     }
